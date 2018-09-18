418ade465e8f80ede316d49840d4e4586dc85373
Derek Lesho <dereklesho52@Gmail.com>
2018-09-17 21:17:57 -0400

    Import Kernel Object Types from ReactOS for Sync functions

--- dlls/ntoskrnl.exe/ntoskrnl.h
+++ dlls/ntoskrnl.exe/ntoskrnl.h
@@ -14,3 +14,35 @@ typedef struct _OBJECT_TYPE
 {
     UNICODE_STRING Name;
 };
+
+/* Kernel Object Types - Taken from ReactOS
+ * https://doxygen.reactos.org/dd/d83/ndk_2ketypes_8h_source.html#l00385*/
+typedef enum _KOBJECTS
+{
+    EventNotificationObject = 0,
+    EventSynchronizationObject = 1,
+    MutantObject = 2,
+    ProcessObject = 3,
+    QueueObject = 4,
+    SemaphoreObject = 5,
+    ThreadObject = 6,
+    GateObject = 7,
+    TimerNotificationObject = 8,
+    TimerSynchronizationObject = 9,
+    Spare2Object = 10,
+    Spare3Object = 11,
+    Spare4Object = 12,
+    Spare5Object = 13,
+    Spare6Object = 14,
+    Spare7Object = 15,
+    Spare8Object = 16,
+    Spare9Object = 17,
+    ApcObject = 18,
+    DpcObject = 19,
+    DeviceQueueObject = 20,
+    EventPairObject = 21,
+    InterruptObject = 22,
+    ProfileObject = 23,
+    ThreadedDpcObject = 24,
+    MaximumKernelObject = 25
+} KOBJECTS;
