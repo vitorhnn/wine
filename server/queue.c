c91add917b374a1dd98f07d4d3647295d4fd7f20
Derek Lesho <dereklesho52@Gmail.com>
2019-06-25 16:23:02 -0400

    user32: Add support for RIDEV_NOLEGACY flag.
    Signed-off-by: Derek Lesho <dereklesho52@Gmail.com>

--- server/queue.c
+++ server/queue.c
@@ -372,6 +372,9 @@ static void set_cursor_pos( struct desktop *desktop, int x, int y )
     static const struct hw_msg_source source = { IMDT_UNAVAILABLE, IMO_SYSTEM };
     struct message *msg;
 
+    if (current->process->rawinput_mouse &&
+        current->process->rawinput_mouse->flags & RIDEV_NOLEGACY) return;
+
     if (!(msg = alloc_hardware_message( 0, source, get_tick_count() ))) return;
 
     msg->msg = WM_MOUSEMOVE;
@@ -1670,6 +1673,9 @@ static int queue_mouse_message( struct desktop *desktop, user_handle_t win, cons
         queue_hardware_message( desktop, msg, 0 );
     }
 
+    if (device && device->flags & RIDEV_NOLEGACY)
+        return FALSE;
+
     for (i = 0; i < ARRAY_SIZE( messages ); i++)
     {
         if (!messages[i]) continue;
@@ -1794,6 +1800,9 @@ static int queue_keyboard_message( struct desktop *desktop, user_handle_t win, c
 
         queue_hardware_message( desktop, msg, 0 );
     }
+
+    if (device && device->flags & RIDEV_NOLEGACY)
+        return FALSE;
 
     if (!(msg = alloc_hardware_message( input->kbd.info, source, time ))) return 0;
     msg_data = msg->data;
